409f8278b5a585224607dcf33473abc216beeb9c
10755373 <45428152+10755373@users.noreply.github.com>
2022-03-16 22:07:25 +0100

    Data cleaning is working
    Data cleaning is working, will try to make lemmatize working as well

--- pipeline/data_cleaning.py
+++ pipeline/data_cleaning.py
@@ -1,8 +1,15 @@
 from curses import def_prog_mode
+<<<<<<< HEAD
+import pyspark.sql.functions as fn
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize, pos_tag
+=======
 from pyspark.sql.functions import * 
 import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk import word_tokenize 
+>>>>>>> 41ac0d1fe7d5490eae779142c59d85a879a8f767
 
 from pyspark.ml.feature import Tokenizer, RegexTokenizer
 from pyspark.sql.functions import col, udf
@@ -13,6 +20,54 @@ from pyspark.sql.types import IntegerType
 nltk.download('punkt')
 nltk.download('wordnet')
 
+<<<<<<< HEAD
+def remove_some_chars(col_name):
+    removed_chars = ("!", "?", '[', ']', '{', '}', '(', ')', '.', '*', '+', '=', '-', '_', "'", ':', ';', '<', '>', "''", '@', '#')
+    regexp = "|".join('\{0}'.format(i) for i in removed_chars)
+    return fn.regexp_replace(col_name, regexp, " ")
+
+
+def cleanup(sdf):
+    for col_name in ['pauthor', 'ptitle', 'journal_full', 'journal']:
+        sdf = sdf.withColumn(col_name, remove_some_chars(col_name))
+        sdf = sdf.withColumn(col_name, fn.lower(col(col_name)))
+        sdf = sdf.withColumn(col_name, fn.trim(col(col_name)))
+    return sdf
+
+def lemmatize(sdf):
+    for col_name in ['ptitle', 'journal_full', 'journal']:
+        # sdf = sdf.withColumn(col_name, lemmatizer(col_name.cast(fn.StringType()))) 
+        sdf = sdf.withColumn(col_name, lemmatizer(sdf.col_name.cast(fn.StringType()))) 
+
+    return sdf
+
+
+
+def lemmatizer(data_str):
+    # expects a string
+    # data_str = sdf.fn.col('ptitle')
+    list_pos = 0
+    cleaned_str = ''
+    lmtzr = WordNetLemmatizer() 
+    text = data_str.split() 
+    tagged_words = pos_tag(text) 
+    for word in tagged_words:
+        if 'v' in word[1].lower():
+            lemma = lmtzr.lemmatize(word[0], pos='v')
+        else:
+            lemma = lmtzr.lemmatize(word[0], pos='n')
+        if list_pos == 0: 
+            cleaned_str = lemma
+        else:
+            cleaned_str = cleaned_str + ' ' + lemma
+        list_pos += 1 
+    return cleaned_str
+
+def clean_data(sdf):
+    sdf = clean_year(sdf)
+    sdf = cleanup(sdf)
+    # sdf = lemmatize(sdf)
+=======
 def swap_author_title(sdf):
     sdf = sdf.withColumn('wordCount_author', size(split(col('pauthor'), ' ')))
     sdf = sdf.select('*',
@@ -41,9 +96,40 @@ def cleanup(sdf):
         sdf = sdf.withColumn(col_name, remove_some_chars(col_name))
         sdf = sdf.withColumn(col_name, lower(col(col_name)))
         sdf = sdf.withColumn(col_name, trim(col(col_name)))
+>>>>>>> 41ac0d1fe7d5490eae779142c59d85a879a8f767
+    return sdf
+
+def check_author(sdf):
+    authorsdf1 = sdf.withColumn('wordCount_author', fn.size(fn.split(fn.col('pauthor'), ' ')))
+    authorsdf1 = authorsdf1.withColumn('aut1_bool', fn.when((authorsdf1.pauthor.contains(r'|') | (fn.col('wordCount_author') < 5)), 1).otherwise(0))
+    authorsdf1 = authorsdf1.withColumn('aut_bool', fn.when((fn.col('aut1_bool') == 1), 1).otherwise(0))   
+    authorsdf1 = authorsdf1.drop('aut1_bool')
+    return authorsdf1
+
+
+def swap_author(sdf):
+    bc = sdf.withColumn('author', fn.when(fn.col('aut_bool') == 1, fn.col('pauthor')).otherwise(fn.col('ptitle')))
+    return bc
+
+
+def swap_title(sdf):
+    bc = sdf.withColumn('title', fn.when(fn.col('aut_bool') == 1, fn.col('ptitle')).otherwise(fn.col('pauthor')))
+    return bc
+
+
+<<<<<<< HEAD
+def swap_values(sdf):
+    sdf = swap_author(sdf)
+    sdf = swap_title(sdf)
     return sdf
 
 
+def clean_year(sdf):
+    sdf = sdf.withColumn('year',fn.ceil(fn.abs(sdf.pyear)))
+    sdf = sdf.drop('pyear')
+    return sdf
+
+=======
 def clean_year(sdf):
     sdf = sdf.withColumn('pyear',ceil(abs(sdf.pyear)))
     return sdf
@@ -79,6 +165,7 @@ def clean_data(sdf):
 #     regexTokenizer = RegexTokenizer(inputCol="pauthor", outputCol="author_char", pattern="\\w")
 #     tokenized = regexTokenizer.transform(sdf)
 #     return tokenized
+>>>>>>> 41ac0d1fe7d5490eae779142c59d85a879a8f767
 
 
 
